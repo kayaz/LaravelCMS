8fb001d8d4be88501b266b4a43aab37427f2321a
Jacek <kontakt@jk-design.pl>
2019-08-16 16:44:58 +0200

    Dodanie chmod do katalogów

--- app/Http/Controllers/AdminSlider/SliderController.php
+++ app/Http/Controllers/AdminSlider/SliderController.php
@@ -65,8 +65,8 @@ class SliderController extends Controller
             // Make thumbs
             $filepath = public_path('storage/slider/' . $name);
             $thumbnailpath = public_path('storage/slider/thumbs/' . $name);
-            $image = Image::make($filepath)->fit(1920, 700)->save($filepath);
             chmod($filepath, 0755);
+            $image = Image::make($filepath)->fit(1920, 700)->save($filepath);
             $image = Image::make($filepath)->resize(200, 200, function ($constraint) {$constraint->aspectRatio();})->save($thumbnailpath);
             chmod($thumbnailpath, 0755);
 
@@ -119,8 +119,8 @@ class SliderController extends Controller
             // Make thumbs
             $filepath = public_path('storage/slider/' . $name);
             $thumbnailpath = public_path('storage/slider/thumbs/' . $name);
-            $image = Image::make($filepath)->fit(1920, 700)->save($filepath);
             chmod($filepath, 0755);
+            $image = Image::make($filepath)->fit(1920, 700)->save($filepath);
             $image = Image::make($filepath)->resize(200, 200, function ($constraint) {$constraint->aspectRatio();})->save($thumbnailpath);
             chmod($thumbnailpath, 0755);
 
